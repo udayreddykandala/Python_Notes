569d1ce360d76df271b8ae4e2e5eeffa69aedc9f
udayreddykandala <136275899+udayreddykandala@users.noreply.github.com>
2024-06-30 16:47:37 +0100

    first commit
    Signed-off-by: udayreddykandala <136275899+udayreddykandala@users.noreply.github.com>

---
+++ Day_01/introduction.py
@@ -0,0 +1,29 @@
+# Q1.Write a Python program to print "Hello, World!"
+
+# Q1: Print "Hello, World!"
+print("Hello, World!")
+
+# Q2: Calculate the sum of two numbers entered by the user
+
+# Prompt the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Prompt the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {sum}")
+
+# Q3: Convert temperature from Celsius to Fahrenheit
+
+# Prompt the user to enter the temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9 / 5) + 32
+
+# Print the result
+print(f"{celsius} Celsius is equal to {fahrenheit} Fahrenheit")
